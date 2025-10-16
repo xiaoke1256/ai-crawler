e8125e31f3d90331e2240b54851df72830c176fd
xiaoke_1256 <xiaoke_1256@sina.com>
2025-10-16 16:51:09 +0800

    run ai task

--- ai_task.py
+++ ai_task.py
@@ -19,21 +19,22 @@ client = MongoClient(MONGODB_URI)
 db = client['crawler']
 commentsCollection = db['comments']
 
-# 获取未打标签的评论
-comments = commentsCollection.find({"emotion": {"$exists": False}})
-for comment in comments:
-    print(f"开始打标签: {comment['text']}")
-    # 模拟打标签
-    #time.sleep(random.uniform(1, 3))
-    result = ai.analyze_sentiment(comment['text'])
-    print(f"情感分析结果: {result}")
-    emotion = ""
-    # 输出主要情感
-    if result["positive"] > result["negative"] and result["positive"] > result["neutral"]:
-        emotion = 'positive'
-    elif result["negative"] > result["positive"] and result["negative"] > result["neutral"]:
-        emotion = 'negative'
-    else:
-        emotion = 'neutrality'
-    # 更新标签
-    commentsCollection.update_one({"_id": comment["_id"]}, {"$set": {"emotion": emotion}})
+def label_comments():
+    # 获取未打标签的评论
+    comments = commentsCollection.find({"emotion": {"$exists": False}})
+    for comment in comments:
+        print(f"开始打标签: {comment['text']}")
+        # 模拟打标签
+        #time.sleep(random.uniform(1, 3))
+        result = ai.analyze_sentiment(comment['text'])
+        print(f"情感分析结果: {result}")
+        emotion = ""
+        # 输出主要情感
+        if result["positive"] > result["negative"] and result["positive"] > result["neutral"]:
+            emotion = 'positive'
+        elif result["negative"] > result["positive"] and result["negative"] > result["neutral"]:
+            emotion = 'negative'
+        else:
+            emotion = 'neutrality'
+        # 更新标签
+        commentsCollection.update_one({"_id": comment["_id"]}, {"$set": {"emotion": emotion}})
--- app.py
+++ app.py
@@ -1,14 +1,30 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from .crawler_task import scrape_website
+from .comments_task import scrape_all_comments
+from .ai_task import label_comments
 import datetime
 import signal
 import sys
 
-def task():
-    print("任务执行")
+# 爬取所有产品
+def scrape_products():
+    scrape_website("https://category.dangdang.com/cp01.41.05.03.00.00.html")
+    print("爬取所有产品任务执行")
+
+def scrape_comments():
+    scrape_all_comments()
+    print("爬取评论任务执行")
+
+def run_ai():
+    label_comments()
 
 # 创建一个调度器实例：BlockingScheduler适用于大多数情况，因为它会阻塞主线程直到所有任务完成。
 scheduler = BlockingScheduler()
-scheduler.add_job(task, 'cron', hour=15, minute=29) # 每10秒执行一次任务
+scheduler.add_job(scrape_products, 'cron', hour=15, minute=29) # 每天15:29执行任务
+
+scheduler.add_job(scrape_comments, 'cron', hour=18, minute=29) # 每天18:29执行任务
+
+scheduler.add_job(label_comments, 'cron', hour=23, minute=29) # 每天23:29执行任务
 scheduler.start()
 
 # def signal_handler(sig, frame):
